9317399674cc2ffdd35f1653ce5b694fc4e825b7
simashun <jdaoqi78@gmail.com>
2025-12-09 19:21:29 +0900

    Add attendance and application migrations

---
+++ src/database/migrations/2025_12_09_100425_add_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('admin_status')->default(false);
+            $table->string('attendance_status')->default('勤務外');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
